fcb59a1fd1a4c08673e602e9f815b59252354e4d
BoppreH <boppreh@gmail.com>
2013-05-22 16:09:25 -0300

    Create server part of user

--- meet.go
+++ meet.go
@@ -4,27 +4,18 @@ import (
     "net/http"
 )
 
-type Single struct {
-    address string
-    lock chan string 
-}
-
-var singles = map[string] Single {}
+var singles = map[string] string {}
 
 func meet(w http.ResponseWriter, r *http.Request) {
     if value, ok := singles[r.URL.Path]; ok {
-        value.lock <- r.RemoteAddr
-        w.Write([]byte(value.address))
+        w.Write([]byte(value))
     } else {
-        single := Single{r.RemoteAddr, make(chan string)}
-        singles[r.URL.Path] = single
-        w.Write([]byte(<- single.lock))
-        delete(singles, r.URL.Path)
+        singles[r.URL.Path] = r.RemoteAddr
+        w.Write([]byte(r.RemoteAddr))
     }
 }
 
 func main() {
     http.Handle("/meet/", http.StripPrefix("/meet/", http.HandlerFunc(meet)))
-	//http.Handle("/", http.RedirectHandler("/view/FrontPage", http.StatusFound))
 	http.ListenAndServe("localhost:8080", nil)
 }
---
+++ server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "net"
+    //"net/http"
+    //"io/ioutil"
+    "bufio"
+    "fmt"
+    "time"
+)
+
+func readSocket(conn net.Conn) {
+    reader := bufio.NewReader(conn)
+    for {
+        fmt.Print(reader.ReadString('\n'))
+    }
+}
+
+func writeSocket(conn net.Conn) {
+    for {
+        var message string
+        _ , _ = fmt.Scanf("%s", &message)
+        fmt.Fprintf(conn, message)
+    }
+}
+
+func handleConnection(conn net.Conn) {
+    go readSocket(conn)
+    go writeSocket(conn)
+}
+
+func main() {
+    var clientPairId string
+    fmt.Print("Enter a unique ID for the client pair: ")
+    _, _ = fmt.Scanf("%s", &clientPairId)
+
+    firstConn, err := net.Dial("tcp", "localhost:8080")
+    if err != nil {
+        // handle error
+    }
+    fmt.Fprintf(firstConn, "GET /meet/" + clientPairId + " HTTP/1.0\r\n\r\n")
+    firstConn.Close()
+    
+    // Block to let the OS free the port.
+    time.Sleep(1 * time.Millisecond)
+
+    ln, err := net.Listen("tcp", firstConn.LocalAddr().String())
+    if err != nil {
+        fmt.Println("Could not open listening socket! " + err.Error())
+        return
+    }
+
+    fmt.Println("Listening on address " + firstConn.LocalAddr().String())
+
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            fmt.Println(err.Error())
+            continue
+        }
+        go handleConnection(conn)
+    }
+}
